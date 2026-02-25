14a6e99d97be1935bf02cd5de9a9cdd2c8ae23d5
saisrijagadish36-cmd <saisrijagadish36@gmail.com>
2026-02-26 00:18:29 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,9 @@
+# functions.py
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+print("Addition:", add_numbers(5, 3))
+print("Multiplication:", multiply_numbers(4, 2))
