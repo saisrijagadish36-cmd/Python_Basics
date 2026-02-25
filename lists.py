e46682eb4d40562919e653c547bb492196b35d5d
saisrijagadish36-cmd <saisrijagadish36@gmail.com>
2026-02-26 00:17:35 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,9 @@
+# lists.py
+numbers = [10, 20, 30, 40, 50]
+
+print("Original list:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+numbers.append(60)
+print("After append:", numbers)
